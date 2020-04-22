30d524e2434587a4bddb1f4bdaea231c24a930d9
chandan <chandan@gmail.com>
2020-04-22 22:17:18 +0530

    Modified to chaitu

--- src/main/java/com/chaitu/CbCustomer1Application.java
+++ src/main/java/com/chaitu/CbCustomer1Application.java
@@ -1,4 +1,4 @@
-package com.chandan;
+package com.chaitu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/chaitu/controller/SampleController.java
+++ src/main/java/com/chaitu/controller/SampleController.java
@@ -1,21 +1,17 @@
-package com.chandan.controller;
+package com.chaitu.controller;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.sql.DataSource;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.ParameterizedPreparedStatementSetter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
